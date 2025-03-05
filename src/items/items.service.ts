8b71db5a3159bfc9056c6eee041413223ab660a4
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-03-04 20:26:10 -0400

    [Items]: update item queries to use UUID and enhance error handling

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateItemInput, UpdateItemInput } from './dto/inputs';
 import { Item } from './entities/item.entity';
 import { Repository } from 'typeorm';
@@ -16,12 +16,18 @@ export class ItemsService {
     return await this.itemsRepository.save(newItem);
   }
 
-  findAll() {
-    return [];
+  async findAll(): Promise<Item[]> {
+    // TODO: filtrar, paginar, por usuario
+
+    return this.itemsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  async findOne(id: string): Promise<Item> {
+    const item = await this.itemsRepository.findOneBy({ id });
+
+    if (!item) throw new NotFoundException(`Item with id: ${id} not found`);
+
+    return item;
   }
 
   update(id: number, updateItemInput: UpdateItemInput) {
