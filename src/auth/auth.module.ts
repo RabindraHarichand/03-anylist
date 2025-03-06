01ea90543331978c324088a38ef01d95a6e240bc
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-03-06 15:54:42 -0400

    [Auth] : integrate JWT authentication with Passport and configuration support

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   providers: [AuthResolver, AuthService],
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '4h',
+          },
+        };
+      },
+    }),
+  ],
 })
 export class AuthModule {}
