e8452ae5874eace822faaf3506cef9185a317c33
Rabindra Harichand <rharichand.17@est.ucab.edu.ve>
2025-03-06 09:47:01 -0400

    [Users]: enhance user creation with password hashing and error handling

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,35 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { UpdateUserInput } from './dto/update-user.input';
 import { User } from './entities/user.entity';
 import { SignupInput } from '../auth/dto/inputs/signup.input';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
+  private logger: Logger = new Logger('UsersService');
+
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
   ) {}
 
   async create(signupInput: SignupInput): Promise<User> {
+    const salt = 10;
     try {
-      const newUser = this.usersRepository.create(signupInput);
-
-      return this.usersRepository.save(newUser);
+      const newUser = this.usersRepository.create({
+        ...signupInput,
+        password: bcrypt.hashSync(signupInput.password, salt),
+      });
+      return await this.usersRepository.save(newUser);
     } catch (error) {
-      console.log(error);
-
-      throw new BadRequestException('Algo salio mal');
+      this.handleDBErrors(error);
     }
   }
 
@@ -38,5 +47,15 @@ export class UsersService {
 
   block(id: string): Promise<User> {
     throw new Error('block not implemented');
+  }
+
+  private handleDBErrors(error: any): never {
+    this.logger.error(error);
+
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail.replace('Key', ''));
+    }
+
+    throw new InternalServerErrorException('Please check server logs');
   }
 }
